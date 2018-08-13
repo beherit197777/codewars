8a9fae068dd9ae5f36046a4d09e7600623e9921f
beherit197777 <beherit197777@gmail.com>
2018-08-13 12:15:02 -0700

    first commit

---
+++ kyu8/number.rb
@@ -0,0 +1,22 @@
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+But can you do it in 1 line of code and without any conditionals?
+
+
+#my solution
+
+def opposite(n)
+ n * -1
+end
+
+#best solution
+
+def opposite n
+  -n
+end
